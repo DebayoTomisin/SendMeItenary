dce9ddd6e7fcb9f3ef2616e4fecf3c47c567058e
Debayo Tomisin <debayotomisin07@gmail.com>
2022-09-25 14:55:43 +0100

    chore/add index & details page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,55 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import NextNProgress from "nextjs-progressbar";
+import { Toaster } from "react-hot-toast";
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { AppProvider } from "context";
+import { ChakraProvider, extendTheme, Box } from "@chakra-ui/react";
+
+const colors = {
+  primary: {
+    one: "#FF1B03",
+    two: "#F6F5F5",
+    text: "#092443",
+    grey: "#CDCCCC",
+    white: "#FFFFFF",
+    lightRed: "#F1D1D1",
+    red2: "#CB4234",
+    red: "#F18479",
+    gray1: "#8F92A1",
+    gray2: "#777E96",
+  },
+};
+
+const sizes = {
+  sizes: {
+    max: "max-content",
+    min: "min-content",
+    full: "100%",
+    screen: "100vh",
+  },
+};
+
+const theme = extendTheme({ colors, sizes });
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NextNProgress
+        startPosition={0.3}
+        color="#FF1B03"
+        height={6}
+        showOnShallow={true}
+      />
+      <AppProvider>
+        <ChakraProvider theme={theme}>
+          <Toaster />
+          <Box pt={8} px="4" bg="primary.two" minH="sizes.screen">
+            <Component {...pageProps} />
+          </Box>
+        </ChakraProvider>
+      </AppProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
